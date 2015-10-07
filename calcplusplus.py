7e93877aa7510db03de8a6903d476b7c35483c5d
carlos escudero garcia <cescuder@zeta40.aulas.gsyc.es>
2015-10-07 10:38:39 +0200

    pep8 pasado

--- calcplusplus.py
+++ calcplusplus.py
@@ -7,38 +7,33 @@ import calcoohija
 import csv
 
 if __name__ == "__main__":
-    
+
     calc = calcoohija.CalculadoraHija()
     with open(sys.argv[1]) as fichero:
         reader = csv.reader(fichero)
         for operandos in reader:
-           
-            print (operandos)
             operacion = operandos[0]
-           
+
             if operacion == "suma":
                 resultado = calc.suma(int(operandos[1]), int(operandos[2]))
                 for numero in operandos[3:]:
                     resultado = calc.suma(int(resultado), int(numero))
-                print(resultado)    
-                    
+                print(resultado)
+
             elif operacion == "resta":
                 resultado = calc.resta(int(operandos[1]), int(operandos[2]))
                 for numero in operandos[3:]:
                     resultado = calc.resta(int(resultado), int(numero))
                 print(resultado)
-                
+
             elif operacion == "multiplica":
                 resultado = calc.producto(int(operandos[1]), int(operandos[2]))
                 for numero in operandos[3:]:
                     resultado = calc.producto(int(resultado), int(numero))
                 print (resultado)
-                
+
             elif operacion == "divide":
                 resultado = calc.division(int(operandos[1]), int(operandos[2]))
                 for numero in operandos[3:]:
                     resultado = calc.division(int(resultado), int(numero))
-                print (resultado)        
-                
-           
-
+                print (resultado)
